cb92b42b7acd5ed6b91571450935dfeda7f0bf7b
Jose Gato <jgato@libresoft.es>
2013-03-03 19:59:46 +0100

    create and test replica set
    last homework

---
+++ mongo/mongoDB.course/hw6.6/create_replica.sh
@@ -0,0 +1,4 @@
+mkdir -p data/rs{1,2,3}
+mongod --replSet m101 --logpath data/1.log --dbpath data/rs1 --port 27017 --fork --oplogSize=50 --smallfiles
+mongod --replSet m101 --logpath data/2.log --dbpath data/rs2 --port 27018 --fork --oplogSize=50 --smallfiles
+mongod --replSet m101 --logpath data/3.log --dbpath data/rs3 --port 27019 --fork --oplogSize=50 --smallfiles
